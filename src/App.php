ff157302d0af18df9a77fec66945e26e50ddfad3
Eric Myers <badllama77@gmail.com>
2017-04-30 22:14:07 -0400

    adding logging setup and connection setup

--- src/App.php
+++ src/App.php
@@ -14,6 +14,11 @@ class App
         $settings = require __DIR__.'/settings.php';
         $this->app = new \Slim\App($settings);
 
+        $container = $this->app->getContainer();
+
+        $this->initLogging($container);
+
+        $this->setUpDatabaseManager();
     }
 
     public function get()
@@ -26,5 +31,35 @@ class App
         $database = $this->app->getContainer()->get('db');
         $database->setAsGlobal();
         $database->bootEloquent();
+    }
+
+    private function initLogging(\Slim\Container $container)
+    {
+        $container['logger'] = function ($c) {
+            $settings = $c->get('settings')['logger'];
+            $logger = new \Monolog\Logger($settings['name']);
+            $logger->pushHandler(new \Monolog\Handler\StreamHandler($settings['path'], \Monolog\Logger::DEBUG));
+
+            return $logger;
+        };
+    }
+
+    private function initDatabase(\Slim\Container $container)
+    {
+        $container['db'] = function ($c) {
+            $manager = new \Illuminate\Database\Capsule\Manager();
+            $config = array_intersect_key($_SERVER, array_flip([
+                'driver',
+                'host',
+                'username',
+                'password',
+                'charset',
+                'collation',
+                'database',
+                'port']));
+            $manager->addConnection($config);
+
+            return $manager;
+        };
     }
 }
