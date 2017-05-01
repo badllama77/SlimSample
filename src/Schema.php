306327f7fe180328b87396917596e5619ebc705c
Eric Myers <badllama77@gmail.com>
2017-04-30 22:26:52 -0400

    adding address and phone tables

--- src/Schema.php
+++ src/Schema.php
@@ -13,6 +13,8 @@ class Schema
     public static function createTables()
     {
         self::createContactsTable();
+        self::createAddressesTable();
+        self::createPhoneNumbersTable();
     }
 
     private static function createContactsTable()
@@ -26,6 +28,46 @@ class Schema
                 $table->string('email');
                 $table->date('updated_at');
                 $table->date('created_at');
+            });
+        }
+    }
+
+    private static function createAddressesTable()
+    {
+        if (!Manager::schema()->hasTable('addresses')) {
+            Manager::schema()->create('addresses', function (Blueprint $table) {
+                $table->increments('id');
+                $table->string('address_type');
+                $table->string('street_line1');
+                $table->string('street_line2');
+                $table->string('city');
+                $table->string('state');
+                $table->string('zipcode');
+                $table->integer('contact_id');
+                $table->date('updated_at');
+                $table->date('created_at');
+                $table->foreign('contact_id')
+                    ->references('id')
+                    ->on('contacts')
+                    ->onDelete('cascade');
+            });
+        }
+    }
+
+    private static function createPhoneNumbersTable()
+    {
+        if (!Manager::schema()->hasTable('phone_numbers')) {
+            Manager::schema()->create('phone_numbers', function (Blueprint $table) {
+                $table->increments('id');
+                $table->string('phone_type');
+                $table->string('number');
+                $table->integer('contact_id');
+                $table->date('updated_at');
+                $table->date('created_at');
+                $table->foreign('contact_id')
+                    ->references('id')
+                    ->on('contacts')
+                    ->onDelete('cascade');
             });
         }
     }
