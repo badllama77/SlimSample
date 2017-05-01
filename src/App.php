8227764848ef303f807ed242d1b0038a512ce7a8
Eric Myers <badllama77@gmail.com>
2017-04-30 22:18:49 -0400

    add init db call

--- src/App.php
+++ src/App.php
@@ -17,6 +17,7 @@ class App
         $container = $this->app->getContainer();
 
         $this->initLogging($container);
+        $this->initDatabase($container);
 
         $this->setUpDatabaseManager();
     }
